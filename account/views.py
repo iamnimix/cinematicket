2fdf478db3f4069452a64115971dec0cdfb9fe38
iamnimix <nima.abarghooie1@gmail.com>
2025-09-28 12:02:26 +0330

    Create RegisterUser API

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
 
+from .serializer import UserSerializer
+
+User = get_user_model()
 # Create your views here.
+
+
+class RegisterUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
