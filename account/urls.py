3256898703239d03e04157688fe0fa36ca8c1cea
iamnimix <nima.abarghooie1@gmail.com>
2025-09-29 17:17:23 +0330

    Add OtpVerification URL

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from account.views import RegisterUser, LoginUser
+from account.views import RegisterUser, LoginUser, OtpVerification
 
 router = DefaultRouter()
 router.register('', LoginUser, basename='users')
+router.register(r'', OtpVerification, basename='otp')
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
