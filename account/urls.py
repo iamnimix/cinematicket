cb6239954df4e0b946d330ca8a43170bbbd8055c
iamnimix <nima.abarghooie1@gmail.com>
2025-09-29 13:47:19 +0330

    Add LoginViewSet URL with Router

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from account.views import RegisterUser
+from account.views import RegisterUser, LoginUser
+
+router = DefaultRouter()
+router.register('', LoginUser, basename='users')
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
+    path('', include(router.urls)),
 ]
