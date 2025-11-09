a7f6b870c5310de2b4adb92946e3053dd30bde67
iamnimix <nima.abarghooie1@gmail.com>
2025-11-09 13:22:57 +0330

    Create a Url for ProfileViewSet

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from account.views import RegisterUser, LoginUser, OtpVerification
+from account.views import RegisterUser, LoginUser, OtpVerification, ProfileViewSet
 
 router = DefaultRouter()
+router.register(r'profile', ProfileViewSet, basename='profile')
 router.register('', LoginUser, basename='users')
 router.register(r'', OtpVerification, basename='otp')
 
